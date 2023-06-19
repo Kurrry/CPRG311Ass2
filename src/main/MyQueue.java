49d56c36f752622c0b032a5646119a218043f141
Kurrry <ajohnsond11@gmail.com>
2023-06-18 22:14:33 -0600

    had a brain fart and forgot to use this LOL. MyQueue no longer runs infinitely

--- src/main/MyQueue.java
+++ src/main/MyQueue.java
@@ -8,10 +8,8 @@ import java.util.NoSuchElementException;
 
 public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
 
-    private MyQueue<E> queue;
-
     public MyQueue() {
-        queue = new MyQueue<>();
+
     }
     @Override
     public boolean hasNext() {
@@ -26,11 +24,7 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
     @Override
     public void enqueue(E toQueue) throws NullPointerException {
         try {
-            if (queue.size() == 0) {
-                queue.add(toQueue);
-            } else {
-                queue.add(queue.size(), toQueue);
-            }
+            this.add(toQueue);
         } catch (NullPointerException ex) {
             ex.printStackTrace();
         }
@@ -39,11 +33,11 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
     @Override
     public E dequeue() throws EmptyQueueException {
         try {
-            if (queue.size() == 0) {
+            if (this.size() == 0) {
                 throw new EmptyQueueException();
             }
-            E element = queue.get(0);
-            queue.remove(0);
+            E element = this.get(0);
+            this.remove(0);
             return element;
         } catch (EmptyQueueException ex) {
             return null;
@@ -53,10 +47,10 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
     @Override
     public E peek() throws EmptyQueueException {
         try {
-            if (queue.size() == 0) {
+            if (this.size() == 0) {
                 throw new EmptyQueueException();
             }
-            return queue.get(0);
+            return this.get(0);
         } catch (EmptyQueueException ex) {
             return null;
         }
@@ -65,10 +59,10 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
     @Override
     public boolean equals(QueueADT<E> otherQueue) {
         int i = 0;
-        Object[] queueArray = queue.toArray();
+        Object[] queueArray = this.toArray();
         Object[] otherArray = otherQueue.toArray();
 
-        while (i < queue.size()) {
+        while (i < this.size()) {
             if (!queueArray[i].equals(otherArray[i])) {
                 return false;
             }
@@ -81,12 +75,12 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
 
     @Override
     public Object[] toArray() {
-        Object[] temp = new Object[queue.size()];
-        Iterator<E> iterator = queue.iterator();
+        Object[] temp = new Object[this.size()];
+        Iterator<E> iterator = this.iterator();
         int i = 0;
 
         while(iterator.hasNext()) {
-            temp[i] = queue.get(i);
+            temp[i] = this.get(i);
             i++;
         }
         return temp;
@@ -97,12 +91,12 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
         if(copy == null || copy.length == 0) {
             throw new NullPointerException();
         }
-        copy = (E[]) new Object[queue.size()];
-        Iterator<E> iterator = queue.iterator();
+        copy = (E[]) new Object[this.size()];
+        Iterator<E> iterator = this.iterator();
         int i = 0;
 
         while(iterator.hasNext()) {
-            copy[i] = queue.get(i);
+            copy[i] = this.get(i);
             i++;
         }
         return copy;
@@ -115,7 +109,7 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size() == 0 && this.getHead() == null && this.getTail() == null;
     }
 
     @Override
@@ -130,28 +124,7 @@ public class MyQueue<E> extends MyDLL<E> implements QueueADT<E>, Iterator<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return new QueueIterator();
-    }
-
-    private class QueueIterator implements Iterator<E> {
-        private MyDLLNode<E> node;
-
-        private QueueIterator() {
-            node = queue.getHead();
-        }
-        @Override
-        public boolean hasNext() {
-            return node.getNextNode() != null;
-        }
-
-        @Override
-        public E next() throws NoSuchElementException {
-            if (hasNext()) {
-                node = node.getNextNode();
-                return node.getElement();
-            } else {
-                throw new NoSuchElementException();
-            }
-        }
+        return super.iterator();
+        //return new QueueIterator();
     }
 }
