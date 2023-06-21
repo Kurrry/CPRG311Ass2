6767da672fd224f32914641b527ff9915a5a6a2b
Mooncaked <ericgagne3321@gmail.com>
2023-06-21 14:59:19 -0600

    Added ArrayList test class

---
+++ test/MyArrayListTests.java
@@ -0,0 +1,207 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Eric
+ *
+ */
+class MyArrayListTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#MyArrayList()}.
+	 */
+	@Test
+	void testMyArrayList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#MyArrayList(int)}.
+	 */
+	@Test
+	void testMyArrayListInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#shiftRight(int)}.
+	 */
+	@Test
+	void testShiftRight() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#shiftLeft(int)}.
+	 */
+	@Test
+	void testShiftLeft() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#increaseCap()}.
+	 */
+	@Test
+	void testIncreaseCap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#size()}.
+	 */
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#clear()}.
+	 */
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#add(int, java.lang.Object)}.
+	 */
+	@Test
+	void testAddIntE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#add(java.lang.Object)}.
+	 */
+	@Test
+	void testAddE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#addAll(utilities.ListADT)}.
+	 */
+	@Test
+	void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#get(int)}.
+	 */
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#remove(int)}.
+	 */
+	@Test
+	void testRemoveInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#remove(java.lang.Object)}.
+	 */
+	@Test
+	void testRemoveE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#set(int, java.lang.Object)}.
+	 */
+	@Test
+	void testSet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#isEmpty()}.
+	 */
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#contains(java.lang.Object)}.
+	 */
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#toArray(E[])}.
+	 */
+	@Test
+	void testToArrayEArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#toArray()}.
+	 */
+	@Test
+	void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#iterator()}.
+	 */
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#hasNext()}.
+	 */
+	@Test
+	void testHasNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyArrayList#next()}.
+	 */
+	@Test
+	void testNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
