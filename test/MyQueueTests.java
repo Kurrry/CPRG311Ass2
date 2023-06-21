54d4f1ae1ec7d25f5557962f875d343363964f90
Mooncaked <ericgagne3321@gmail.com>
2023-06-21 12:42:12 -0600

    Added queue test class and package for JUnit (might have to add library to project)

---
+++ test/MyQueueTests.java
@@ -0,0 +1,151 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Eric
+ *
+ */
+class MyQueueTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#hasNext()}.
+	 */
+	@Test
+	void testHasNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#next()}.
+	 */
+	@Test
+	void testNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#size()}.
+	 */
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#isEmpty()}.
+	 */
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#toArray(E[])}.
+	 */
+	@Test
+	void testToArrayEArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#toArray()}.
+	 */
+	@Test
+	void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#iterator()}.
+	 */
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#MyQueue()}.
+	 */
+	@Test
+	void testMyQueue() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#enqueue(java.lang.Object)}.
+	 */
+	@Test
+	void testEnqueue() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#dequeue()}.
+	 */
+	@Test
+	void testDequeue() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#peek()}.
+	 */
+	@Test
+	void testPeek() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#equals(utilities.QueueADT)}.
+	 */
+	@Test
+	void testEqualsQueueADTOfE() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#isFull()}.
+	 */
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.MyQueue#dequeueAll()}.
+	 */
+	@Test
+	void testDequeueAll() {
+		fail("Not yet implemented");
+	}
+
+}
