5a9461a0fe7c1aa4cc39fd98a3094db9ff2e634d
Kurrry <ajohnsond11@gmail.com>
2023-06-22 10:58:57 -0600

    Removed redundant public tags in QueueADT and StackADT, as interface methods are always public.

--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -16,7 +16,7 @@ public interface StackADT<E> {
      * @param toPush Item to be placed on top of the stack.
      * @throws NullPointerException when the item to be placed is null.
      */
-    public void push(E toPush) throws NullPointerException;
+    void push(E toPush) throws NullPointerException;
 
     /**
      * Removes and returns the top element of the stack.
@@ -24,7 +24,7 @@ public interface StackADT<E> {
      * @return The item removed from the top of the stack.
      * @throws EmptyStackException when attempting to pop an empty stack.
      */
-    public E pop() throws EmptyStackException;
+    E pop() throws EmptyStackException;
 
     /**
      * Returns the top element of the stack.
@@ -32,7 +32,7 @@ public interface StackADT<E> {
      * @return The top element of the stack.
      * @throws EmptyStackException when attempting to peek into an empty stack.
      */
-    public E peek() throws EmptyStackException;
+    E peek() throws EmptyStackException;
 
     /**
      * Checks if another stack is equal to this one. Stacks must have the same
@@ -41,14 +41,14 @@ public interface StackADT<E> {
      * @param otherStack The other stack to be checked for equality.
      * @return True if both stacks are identical. False otherwise.
      */
-    public boolean equals(StackADT<E> otherStack);
+    boolean equals(StackADT<E> otherStack);
 
     /**
      * Returns an Iterator which iterates over the stack's elements in order.
      *
      * @return The Iterator which iterates over the stack's elements.
      */
-    public Iterator<E> iterator();
+    Iterator<E> iterator();
 
     /**
      * Copies all the stack's elements into an Object array. The top of the stack is
@@ -56,7 +56,7 @@ public interface StackADT<E> {
      *
      * @return The Object array containing all the stack's elements in order.
      */
-    public Object[] toArray();
+    Object[] toArray();
 
     /**
      * Copies all the stack's elements into an array. The top of the stack is
@@ -68,7 +68,7 @@ public interface StackADT<E> {
      * @return The E array containing all the stack's elements in order.
      * @throws NullPointerException when the specified array is null
      */
-    public E[] toArray(E[] copy) throws NullPointerException;
+    E[] toArray(E[] copy) throws NullPointerException;
 
     /**
      * Returns the position of the specified element. Starts with 1 at the top of the stack.
@@ -77,7 +77,7 @@ public interface StackADT<E> {
      * @return The position of the specified element. The top of the stack is position 1,
      *         the next is position 2, etc. -1 is returned if the element isn't in the stack.
      */
-    public int search(E obj);
+    int search(E obj);
 
     /**
      * Returns whether the specified element exists in the stack.
@@ -86,24 +86,24 @@ public interface StackADT<E> {
      * @return True if the element is in the stack. False if not.
      * @throws NullPointerException when the specified element is null.
      */
-    public boolean contains(E obj) throws NullPointerException;
+    boolean contains(E obj) throws NullPointerException;
 
     /**
      * Returns the number of elements in the stack.
      *
      * @return The current size of the stack.
      */
-    public int size();
+    int size();
 
     /**
      * Checks if the stack is empty.
      *
      * @return True if the stack has no elements. False otherwise.
      */
-    public boolean isEmpty();
+    boolean isEmpty();
 
     /**
      * Clears all items in the stack from top to bottom.
      */
-    public void clear();
+    void clear();
 }
